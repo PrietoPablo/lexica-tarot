4f79fe299331f60701f23b5167fbf8855c87ea93
PrietoPablo <prietopablo88@gmail.com>
2022-10-11 15:13:45 +0200

    connected to lexica API with exemple query

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,18 @@
+import axios from 'axios';
 // == Composant
 function App() {
+  const handleClick = () => {
+    axios.get('https://lexica.art/api/v1/search?q=apples')
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
     <div className="app">
+      <button type="button" className="axios-query" onClick={handleClick}>query</button>
     </div>
   );
 }
