43f51a9e5bacdbfb3a3e7118eb7bd4d2fefd2031
PrietoPablo <prietopablo88@gmail.com>
2022-10-11 17:25:41 +0200

    monocomponent with a form element, display a fetched picture when submitted

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,56 @@
 import axios from 'axios';
-// == Composant
+
+import { useState } from 'react';
+
 function App() {
-  const handleClick = () => {
-    axios.get('https://lexica.art/api/v1/search?q=apples')
+  const readers = [
+    'Cezanne',
+    'Georgia O’Keeffe',
+    'Francisco de Goya'
+  ];
+
+  const [tarotReader, setTarotReader] = useState('');
+  const [cardImageURL, setCardImageURL] = useState('');
+
+  const fetchAPI = (tarotReaderValue) => {
+    axios.get('https://lexica.art/api/v1/search', {
+      params: {
+        q: `the tower in the style of ${tarotReaderValue}`
+      }
+    })
       .then((response) => {
         console.log(response);
+        setCardImageURL(response.data.images[0].src);
       })
       .catch((error) => {
         console.error(error);
       });
   };
+
+  const handleChange = (event) => {
+    setTarotReader(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchAPI(tarotReader);
+  };
+
   return (
     <div className="app">
-      <button type="button" className="axios-query" onClick={handleClick}>query</button>
+      {!cardImageURL
+        ? (
+          <form className="card-reader-form" onSubmit={handleSubmit}>
+            <select value={tarotReader} onChange={handleChange}>
+              <option value="" disabled selected hidden>Select your Reader</option>
+              {readers.map((reader) => (
+                <option key={reader} value={reader}>{reader}</option>
+              ))}
+            </select>
+            <button type="submit">Choose</button>
+          </form>
+        )
+        : <img src={cardImageURL} alt="an apple" className="card-image" />}
     </div>
   );
 }
